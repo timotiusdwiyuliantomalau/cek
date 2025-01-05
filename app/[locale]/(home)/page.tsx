30ea4af9df1e5fc8bbb2edf6b260a7958ea4b9e3
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2025-01-05 19:15:18 +0700

    Update

--- app/[locale]/(home)/page.tsx
+++ app/[locale]/(home)/page.tsx
@@ -1,40 +1,40 @@
-import BrowsingHistoryList from '@/components/shared/browsing-history-list'
-import { HomeCard } from '@/components/shared/home/home-card'
-import { HomeCarousel } from '@/components/shared/home/home-carousel'
-import ProductSlider from '@/components/shared/product/product-slider'
-import { Card, CardContent } from '@/components/ui/card'
+import BrowsingHistoryList from "@/components/shared/browsing-history-list";
+import { HomeCard } from "@/components/shared/home/home-card";
+import { HomeCarousel } from "@/components/shared/home/home-carousel";
+import ProductSlider from "@/components/shared/product/product-slider";
+import { Card, CardContent } from "@/components/ui/card";
 
 import {
   getProductsForCard,
   getProductsByTag,
   getAllCategories,
-} from '@/lib/actions/product.actions'
-import { getSetting } from '@/lib/actions/setting.actions'
-import { toSlug } from '@/lib/utils'
-import { getTranslations } from 'next-intl/server'
+} from "@/lib/actions/product.actions";
+import { getSetting } from "@/lib/actions/setting.actions";
+import { toSlug } from "@/lib/utils";
+import { getTranslations } from "next-intl/server";
 
 export default async function HomePage() {
-  const t = await getTranslations('Home')
-  const { carousels } = await getSetting()
-  const todaysDeals = await getProductsByTag({ tag: 'todays-deal' })
-  const bestSellingProducts = await getProductsByTag({ tag: 'best-seller' })
+  const t = await getTranslations("Home");
+  const { carousels } = await getSetting();
+  const todaysDeals = await getProductsByTag({ tag: "todays-deal" });
+  const bestSellingProducts = await getProductsByTag({ tag: "best-seller" });
 
-  const categories = (await getAllCategories()).slice(0, 4)
+  const categories = (await getAllCategories()).slice(0, 4);
   const newArrivals = await getProductsForCard({
-    tag: 'new-arrival',
-  })
+    tag: "new-arrival",
+  });
   const featureds = await getProductsForCard({
-    tag: 'featured',
-  })
+    tag: "featured",
+  });
   const bestSellers = await getProductsForCard({
-    tag: 'best-seller',
-  })
+    tag: "best-seller",
+  });
   const cards = [
     {
-      title: t('Categories to explore'),
+      title: t("Categories to explore"),
       link: {
-        text: t('See More'),
-        href: '/search',
+        text: t("See More"),
+        href: "/search",
       },
       items: categories.map((category) => ({
         name: category,
@@ -43,45 +43,45 @@ export default async function HomePage() {
       })),
     },
     {
-      title: t('Explore New Arrivals'),
+      title: t("Explore New Arrivals"),
       items: newArrivals,
       link: {
-        text: t('View All'),
-        href: '/search?tag=new-arrival',
+        text: t("View All"),
+        href: "/search?tag=new-arrival",
       },
     },
     {
-      title: t('Discover Best Sellers'),
+      title: t("Discover Best Sellers"),
       items: bestSellers,
       link: {
-        text: t('View All'),
-        href: '/search?tag=new-arrival',
+        text: t("View All"),
+        href: "/search?tag=new-arrival",
       },
     },
     {
-      title: t('Featured Products'),
+      title: t("Featured Products"),
       items: featureds,
       link: {
-        text: t('Shop Now'),
-        href: '/search?tag=new-arrival',
+        text: t("Shop Now"),
+        href: "/search?tag=new-arrival",
       },
     },
-  ]
+  ];
 
   return (
     <>
       <HomeCarousel items={carousels} />
-      <div className='md:p-4 md:space-y-4 bg-border'>
+      <div className="md:p-4 md:space-y-4 bg-border">
         <HomeCard cards={cards} />
-        <Card className='w-full rounded-none'>
-          <CardContent className='p-4 items-center gap-3'>
+        <Card className="w-full rounded-none">
+          <CardContent className="p-4 items-center gap-3">
             <ProductSlider title={t("Today's Deals")} products={todaysDeals} />
           </CardContent>
         </Card>
-        <Card className='w-full rounded-none'>
-          <CardContent className='p-4 items-center gap-3'>
+        <Card className="w-full rounded-none">
+          <CardContent className="p-4 items-center gap-3">
             <ProductSlider
-              title={t('Best Selling Products')}
+              title={t("Best Selling Products")}
               products={bestSellingProducts}
               hideDetails
             />
@@ -89,9 +89,9 @@ export default async function HomePage() {
         </Card>
       </div>
 
-      <div className='p-4 bg-background'>
+      <div className="p-4 bg-background">
         <BrowsingHistoryList />
       </div>
     </>
-  )
+  );
 }
--- lib/actions/setting.actions.ts
+++ lib/actions/setting.actions.ts
@@ -1,69 +1,59 @@
-"use server";
-import { ISettingInput } from "@/types";
-import data from "../data";
-import Setting from "../db/models/setting.model";
-import { connectToDatabase } from "../db";
-import { formatError } from "../utils";
-import { cookies } from "next/headers";
+'use server'
+import { ISettingInput } from '@/types'
+import data from '../data'
+import Setting from '../db/models/setting.model'
+import { connectToDatabase } from '../db'
+import { formatError } from '../utils'
+import { cookies } from 'next/headers'
 
 const globalForSettings = global as unknown as {
-  cachedSettings: ISettingInput | null;
-};
+  cachedSettings: ISettingInput | null
+}
 export const getNoCachedSetting = async (): Promise<ISettingInput> => {
-  await connectToDatabase();
-  const setting = await Setting.findOne();
-  return JSON.parse(JSON.stringify(setting)) as ISettingInput;
-};
+  await connectToDatabase()
+  const setting = await Setting.findOne()
+  return JSON.parse(JSON.stringify(setting)) as ISettingInput
+}
 
 export const getSetting = async (): Promise<ISettingInput> => {
   if (!globalForSettings.cachedSettings) {
-    await connectToDatabase();
-    const setting = await Setting.findOne().lean();
+    console.log('hit db')
+    await connectToDatabase()
+    const setting = await Setting.findOne().lean()
     globalForSettings.cachedSettings = setting
       ? JSON.parse(JSON.stringify(setting))
-      : data.settings[0];
+      : data.settings[0]
   }
-  if(globalForSettings.cachedSettings){
-    globalForSettings.cachedSettings.site.name="TimZone";
-    globalForSettings.cachedSettings.site.slogan="Spend less, enjoy more.";
-    globalForSettings.cachedSettings.site.description="Timzone is a sample Ecommerce website built with Next.js, Tailwind CSS, and MongoDB.";
-    globalForSettings.cachedSettings.site.keywords="Next Ecommerce, Next.js, Tailwind CSS, MongoDB";
-    globalForSettings.cachedSettings.site.email="admin@example.com";
-    globalForSettings.cachedSettings.site.phone="+628121932483";
-    globalForSettings.cachedSettings.site.author="TimZone";
-    globalForSettings.cachedSettings.site.copyright="2024, TimZone.com";
-    globalForSettings.cachedSettings.site.address="Indonesia";
-  }
-  return globalForSettings.cachedSettings as ISettingInput;
-};
+  return globalForSettings.cachedSettings as ISettingInput
+}
 
 export const updateSetting = async (newSetting: ISettingInput) => {
   try {
-    await connectToDatabase();
+    await connectToDatabase()
     const updatedSetting = await Setting.findOneAndUpdate({}, newSetting, {
       upsert: true,
       new: true,
-    }).lean();
+    }).lean()
     globalForSettings.cachedSettings = JSON.parse(
       JSON.stringify(updatedSetting)
-    ); // Update the cache
+    ) // Update the cache
     return {
       success: true,
-      message: "Setting updated successfully",
-    };
+      message: 'Setting updated successfully',
+    }
   } catch (error) {
-    return { success: false, message: formatError(error) };
+    return { success: false, message: formatError(error) }
   }
-};
+}
 
 // Server action to update the currency cookie
 export const setCurrencyOnServer = async (newCurrency: string) => {
-  "use server";
-  const cookiesStore = await cookies();
-  cookiesStore.set("currency", newCurrency);
+  'use server'
+  const cookiesStore = await cookies()
+  cookiesStore.set('currency', newCurrency)
 
   return {
     success: true,
-    message: "Currency updated successfully",
-  };
-};
+    message: 'Currency updated successfully',
+  }
+}
